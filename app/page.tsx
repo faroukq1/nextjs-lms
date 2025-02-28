fb175a3a09236f95d4ebc872aacd2dcf63c8b881
farouk ouledmeriem <126769881+faroukq1@users.noreply.github.com>
2025-02-28 14:10:37 +0100

    remake project

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl">hello world</h1>
-    </div>
-  );
-}
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,14 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
